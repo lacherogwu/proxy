4000eda5668cb51fb69ef8a92d2b8285894d7ee9
lachero <asaf@gwumkt.com>
2021-01-05 21:48:39 +0200

    added zapier

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 
 app.set('trust proxy', true);
 
+app.use(express.json());
+
 app.get('/healthcheck', (req, res) => {
     console.log(req.ip);
     res.status(200).json({ success: true, ip: req.ip });
+});
+
+app.post('/zapier', async (req, res) => {
+
+    const { username, password } = req.body;
+
+    const { data } = await axios.post('https://hooks.zapier.com/hooks/catch/5292946/ocfixxv/', {
+        username,
+        password
+    });
+
+    res.json(data);
 });
 
 
